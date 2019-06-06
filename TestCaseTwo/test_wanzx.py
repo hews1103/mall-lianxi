01cd4c8b468d5a3ac2f09d71540eab658cd8e56e
何文双 <1255790661@qq.com>
2019-06-06 15:57:48 +0800

    6.6 下午

--- TestCaseTwo/test_wanzx.py
+++ TestCaseTwo/test_wanzx.py
@@ -1,26 +1,39 @@
 import pytest
 import allure
-from Common import Assert,Request,Login,Tools
+from Common import Assert,Request,Login,Tools,read_excel
 
 assertions = Assert.Assertions()
 request = Request.Request()
 pwd_normal = Tools.random_str_abc(3) + Tools.random_123(3)
 url = Login.url
 
-
+excel_list = read_excel.read_excel_list('./document/商品分类.xlsx')
+ids_list = []
+for i in range(len(excel_list)):
+    # 删除excel_list中每个小list的最后一个元素,并赋值给ids_pop
+    ids_pop = excel_list[i].pop()
+    # 将ids_pop添加到 ids_list 里面
+    ids_list.append(ids_pop)
 
 @allure.feature('注册/登录模块')
 class Test_user():
 
-    @allure.story('注册接口')
-    def test_signup(self):
+    @allure.story('注册接口1')
+    def test_signup1(self):
         user_sig_resp = request.post_request(url=url + 'user/signup',
                                             json = {"phone": Tools.phone_num(), "pwd":pwd_normal , "rePwd": pwd_normal,
                                                   "userName": Tools.random_str_abc(2)+Tools.random_123(1)})
 
         resp_json = user_sig_resp.json()
-        resp_base_1 = resp_json['respBase']
-        assertions.assert_in_text(resp_base_1['respDesc'],'成功')
+
+        assertions.assert_in_text(resp_json['respDesc'],'成功')
+
+    @allure.story('注册接口2')
+    @pytest.mark.parametrize('phone,pwd,rePwd,userName,numb', excel_list, ids=ids_list)
+    def test_signup2(self, phone, pwd, rePwd, userName, numb):
+        user_sig_resp = request.post_request(url=url + 'user/signup',
+                                             json={"phone": phone, "pwd": pwd, "rePwd": rePwd,
+                                                   "userName": userName})
 
 
 
